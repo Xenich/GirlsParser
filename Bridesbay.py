56c898987cc5450f7c48b4bf39cdb68aa8ed0973
Dmitry <xenich@gmail.com>
2019-10-14 17:18:32 +0300

    Улучшена структура кода

--- Bridesbay.py
+++ Bridesbay.py
@@ -1,29 +1,41 @@
 from bs4 import BeautifulSoup
-import urllib3
-import urllib.request
 import concurrent.futures
 import threading
 import os
 import requests
 import time
-import sys
-from pympler import muppy
-from multiprocessing.dummy import Pool as ThreadPool
+from Girl import Girl
+import random
+
+#import urllib3
+#import urllib.request
+#import sys
+#from pympler import muppy
+#from multiprocessing.dummy import Pool as ThreadPool
 
 class Bridesbay():
-
-	def __init__(self, maxWorkers, max_workersPhoto, headers):		# maxWorkers - количество потоков, max_workersPhoto - количество потоков в которые скачиваются фотографии одной девушки
+	'''Класс, реализующий парсинг сайта https://bridesbay.com/
+	:param maxWorkers:int: Количество потоков
+	:param max_workersPhoto:int: Количество потоков в которые скачиваются фотографии одной девушки
+	:param headers:set: Set of headers
+	:param proxy:set: Set of proxys'''
+	def __init__(self, maxWorkers, max_workersPhoto, headers, proxy):		# maxWorkers - количество потоков, max_workersPhoto - количество потоков в которые скачиваются фотографии одной девушки
+		
 		self.lock = threading.RLock()		# мьютекс
 		self.maxWorkers = maxWorkers
 		self.max_workersPhoto = max_workersPhoto
 		self.headers = headers
+		self.proxy = proxy
 		self.dictIdGirl={}			# словарь: Id - Girl
 		self.pageCounter=1
 		self.BridesbayFolder = r'Parsed_data\BridesbayFolder' 
 		self.weAreWorking = True
 
-		# потокобезопасное добавление гёрлы в словарь
-	def AddGirl(self, key, value):
+
+	def AddGirl(self, key, value):				# потокобезопасное добавление гёрлы в словарь
+		'''Потокобезопасное добавление гёрлы в словарь
+		:param key:string: ключ
+		:param value:Girl: значение'''
 		with self.lock:
 			self.dictIdGirl[key] = value
 
@@ -31,11 +43,11 @@ class Bridesbay():
 	def StartParse(self):
 		'''Начинает парсить сайт bridesbay.com'''
 		print("Bridesbay parser is started")	
-
+		Girl.headers = self.headers
 		if not os.path.exists(self.BridesbayFolder):
 			os.makedirs(self.BridesbayFolder)			# создание папки
 		#with ThreadPool(self.maxWorkers) as executor:	
-		with concurrent.futures.ThreadPoolExecutor(max_workers=self.maxWorkers) as executor:						
+		with concurrent.futures.ThreadPoolExecutor(max_workers=self.maxWorkers) as executor:
 			while self.weAreWorking:
 				url = "https://bridesbay.com/women-list/page" + str(self.pageCounter)				
 				soup = self.GetSoup(url)	
@@ -47,13 +59,13 @@ class Bridesbay():
 					try:
 						executor.map(self.ParseGirlTagInThread, girlBlock)	
 						self.pageCounter+=1
-						print("Pages: " + str(self.pageCounter) + "\r\n")
+						print("\r\n\r\n\r\nPAGES: " + str(self.pageCounter) + "\r\n\r\n\r\n")
 						#print(str(len(muppy.get_objects())))
 
 					except Exception as e:
-						print ("\r\n*************    ОШИБКА !!!    **************\r\n")
+						print ("\r\n\r\n\r\n*************    ОШИБКА !!!    **************\r\n")
 						print(e.__class__)
-						print( "\r\n" + e.__doc__ + "\r\n" + e.__str__())
+						print("\r\n" + e.__doc__ + "\r\n" + e.__str__())
 						print(str(self.pageCounter))
 						time.sleep(0.1)	
 						#print(str(len(muppy.get_objects())))
@@ -64,7 +76,10 @@ class Bridesbay():
 		self.weAreWorking = False
 
 		# Метод, отправляемый в поток
-	def ParseGirlTagInThread(self, girlTag):	
+	def ParseGirlTagInThread(self, girlTag):
+		"""Метод, работающий в отдельном потоке
+		:param girlTag: объект тег BeautifulSoup 
+		"""	
 		ID = girlTag.select("div.id")
 		id = ID[0].string						# получили строку вида: User ID: 484969
 		id=id.split(':')[1].strip()
@@ -77,53 +92,52 @@ class Bridesbay():
 			self.ParseGirl(id, girlUrl, girlName, girlAge)
 
 	def ParseGirl (self, id, url, name, age):
-		girl = Girl()	
-		self.AddGirl(id, girl)
-		girl.id = id
-		girl.URL = url	
-		girl.name = name
-		girl.age = age
 		soup = self.GetSoup(url)
 		profileData = soup.select("div.girlGalleryProfile-data")
 		span = profileData[0].findAll("span")
 
-		girl.country =	self.FindTextInTags(span, "Country:")
-		girl.city =	self.FindTextInTags(span, "City:")
-		girl.marital_status = self.FindTextInTags(span, "Marital status:")
-		girl.children = self.FindTextInTags(span, "Children:")
-		girl.height = self.FindTextInTags(span, "Height:")
-		girl.weight = self.FindTextInTags(span, "Weight:")
-		girl.eyeColor = self.FindTextInTags(span, "Eye color:")
-		girl.hairColor = self.FindTextInTags(span, "Hair color:")
-		girl.bodyType = self.FindTextInTags(span, "Body type")
-		girl.religion = self.FindTextInTags(span, "Religion:")
-		girl.education = self.FindTextInTags(span, "Education:")
-		girl.smoke = self.FindTextInTags(span, "Smoke:")
-		girl.drink = self.FindTextInTags(span, "Drink:")
-		girl.englishLevel = self.FindTextInTags(span, "English level:")	
-		girl.occupation =	self.FindTextInTags(span, "Occupation:")	 
+		country =	self.FindTextInTags(span, "Country:")
+		city =	self.FindTextInTags(span, "City:")
+		marital_status = self.FindTextInTags(span, "Marital status:")
+		children = self.FindTextInTags(span, "Children:")
+		height = self.FindTextInTags(span, "Height:")
+		weight = self.FindTextInTags(span, "Weight:")
+		eyeColor = self.FindTextInTags(span, "Eye color:")
+		hairColor = self.FindTextInTags(span, "Hair color:")
+		bodyType = self.FindTextInTags(span, "Body type")
+		religion = self.FindTextInTags(span, "Religion:")
+		education = self.FindTextInTags(span, "Education:")
+		smoke = self.FindTextInTags(span, "Smoke:")
+		drink = self.FindTextInTags(span, "Drink:")
+		englishLevel = self.FindTextInTags(span, "English level:")	
+		occupation =	self.FindTextInTags(span, "Occupation:")	 
 
 		rightCol = soup.select("div.right-col")
 		titles = rightCol[0].select("div.title")
-		girl.aboutMyself = self.FindTextInTags(titles, "About myself")
-		girl.aboutMyPartner = self.FindTextInTags(titles, "About my partner")
-		girl.hobbies = self.FindTextInTags(titles, "Hobbies")
-		girl.ageCriteria = self.FindTextInTags(titles, "Age criteria")
-		girl.datingGoal = self.FindTextInTags(titles, "Dating goal")
+		aboutMyself = self.FindTextInTags(titles, "About myself")
+		aboutMyPartner = self.FindTextInTags(titles, "About my partner")
+		hobbies = self.FindTextInTags(titles, "Hobbies")
+		ageCriteria = self.FindTextInTags(titles, "Age criteria")
+		datingGoal = self.FindTextInTags(titles, "Dating goal")
 
 		photoBlockSoup = soup.find(True, {'class':['block-receptacle', 'photo']})
-		girl.photoMainURL = 'https://bridesbay.com' + photoBlockSoup.select('a.photo-main')[0].get('href')
-		self.dictIdGirl[id] = girl
+		photoMainURL = 'https://bridesbay.com' + photoBlockSoup.select('a.photo-main')[0].get('href')
+
 		photoList = photoBlockSoup.findAll(rel="girlGalleryProfile-photo")
+		photoPholder=[]
 		for photo in photoList:
-			girl.photoPholder.append('https://bridesbay.com' + photo.find('img').get('src')) 
+			photoPholder.append('https://bridesbay.com' + photo.find('img').get('src')) 	
+
+		girl = Girl(id, url, name, age, country, city, marital_status, children, height, weight, eyeColor, hairColor, bodyType, religion, education, smoke, drink, englishLevel, occupation, aboutMyself, aboutMyPartner, hobbies, ageCriteria, datingGoal, photoMainURL, photoPholder)
 		girl.SaveToFolder(self.BridesbayFolder, self.max_workersPhoto)
+		self.AddGirl(id, girl)
+
 		print('Girl ' + girl.name + ' parsed')
 
 		# получение тега по УРЛ
 	def GetSoup(self, url):
 		print("Start get HTTP")
-		html = requests.get(url).text 		#Fetch HTML Page
+		html = requests.get(url, headers=random.sample(self.headers, 1)[0] ).text 		#Fetch HTML Page
 		print("Get HTTP")
 		return BeautifulSoup(html, 'html.parser')
 
@@ -136,81 +150,3 @@ class Bridesbay():
 	def SaveAllGirls(self):
 		for girl in self.dictIdGirl.values():
 			girl.SaveToFolder(self.BridesbayFolder, self.max_workersPhoto)
-
-
-class Girl():
-	def __init__(self):
-		self.id=""
-		self.URL = ""
-		self.name=""
-		self.age=""
-		self.country=""
-		self.city=""
-		self.marital_status=""
-		self.children=""
-		self.height=""
-		self.weight=""
-		self.eyeColor=""
-		self.hairColor=""
-		self.bodyType=""
-		self.religion=""
-		self.education=""
-		self.smoke=""
-		self.drink=""
-		self.englishLevel=""	
-		self.occupation=""	
-		self.aboutMyself="" 
-		self.aboutMyPartner=""
-		self.hobbies=""
-		self.ageCriteria=""
-		self.datingGoal=""
-		self.photoMainURL=""
-		self.photoPholder=[]
-	
-	def SaveToFolder(self, folder, max_workersPhoto):
-		info =  "ID:" + str(self.id) + ". Name: " + str(self.name)+"; age: " + str(self.age) + "; country: " + str(self.country) + "; city: " + str(self.city) + "\r\n"
-		info += "Marital status: " + str(self.marital_status) + "; children: " + str(self.children) + "; height: " + str(self.height) + "; weight: " + str(self.weight) + "; eyeColor: " + str(self.eyeColor) + "; hairColor: " + str(self.hairColor) + "\r\n" 
-		info += "Body type : " + str(self.bodyType) + "; religion: " + str(self.religion) + "; education: " + str(self.education) + "; smoke: " + str(self.smoke) + "; drink: " + str(self.drink) + "; english level: " + str(self.englishLevel) + "; occupation: " + str(self.occupation) + "\r\n"
-		info += "About myself:\r\n"
-		info += str(self.aboutMyself) + "\r\n\r\n"
-		info += "About my partner:\r\n"
-		info += str(self.aboutMyPartner) + "\r\n\r\n"
-		info += "Hobbies: " + str(self.hobbies) + "; age criteria: " + str(self.ageCriteria) + "; dating goal: " + str(self.datingGoal) + "\r\n"
-		info += "Main photo URL: " + str(self.photoMainURL) + "\r\n"
-		info += "URL: " + str(self.URL) + "\r\n"
-		girlFolder = folder + "\\" + self.id
-		if not os.path.exists(girlFolder):
-			os.makedirs(girlFolder)	
-		f=open(girlFolder + "\\"+ self.id +".txt", 'wt')	
-		f.write(info)	
-		f.close()
-			# сохраняем фотографии
-		photosList = []			# список кортежей типа [(photoUrl1, Path1), (photoUrl2, Path2), (photoUrl3, Path3), ... ]
-		photosList.append((self.photoMainURL, girlFolder + "\\" + self.id +"_main.jpg"))		# добавляем в список кортеж главной фотографии
-		counter=1			# счётчик фотографий. будет использоваться в названии файла
-		for photoUrl in self.photoPholder:
-			photoCortege = (photoUrl, girlFolder + "\\" + self.id +"_" + str(counter) + ".jpg")
-			photosList.append(photoCortege)
-			counter+=1		
-		with concurrent.futures.ThreadPoolExecutor(max_workersPhoto) as executor:		# реализация сохранения фотографий с использованием многопоточности
-			executor.map(self.SaveImageFromTuple, tuple(photosList))
-
-		'''		# реализация сохранения фотографий без потоков (по очереди)
-		for photoUrl in self.photoPholder:
-			self.SaveImage(photoUrl, girlFolder + "\\" + self.id +"_" + str(counter) + ".jpg")
-			counter+=1
-		'''
-
-	def SaveImage(self, URL, path):
-		img =	requests.get(URL)				# получили бинарные данные
-		with open(path, 'wb') as f:
-			f = open(path, "wb")
-			f.write(img.content)
-			print(self.id + ": PhotoSaved")
-
-	def SaveImageFromTuple(self, tup):		# tup - tuple вида (photoUrl, path)
-		img =	requests.get(tup[0])				# получили бинарные данные
-		with open(tup[1], 'wb') as f:
-			f = open(tup[1], "wb")
-			f.write(img.content)
-			print(self.id + ": PhotoSaved")
--- Parser.py
+++ Parser.py
@@ -1,7 +1,8 @@
-import Bridesbay
 import gc
 import time
 from threading import Thread
+from Bridesbay import Bridesbay
+
 
 '''
 def gc_cb(phase, info):
@@ -12,8 +13,15 @@ def gc_cb(phase, info):
 
 gc.callbacks.append(gc_cb)
 	'''
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}
-b = Bridesbay.Bridesbay(20, 5, headers)
+h1={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}
+h2={'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'}
+headers = [h1, h2]
+
+proxy=[]
+
+proxy = () 		# множество
+b = Bridesbay(20, 5, headers, proxy)
+#b.AddGirl(,)
 Thread(target=b.StartParse).start()
 time.sleep(30)
 b.StopParse()
